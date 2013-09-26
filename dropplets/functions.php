d9fe134d1d6b00d485685a8e308eab2ffafdb1a5
Kazuhiro Inari <inarinlab@gmail.com>
2013-09-27 02:05:56 +0900

    fix for cache path.

--- dropplets/functions.php
+++ dropplets/functions.php
@@ -305,13 +305,18 @@ define('IS_HOME', $is_home);
 function get_twitter_profile_img($username) {
 	
 	// Get the cached profile image.
-	$profile_image = './cache/'.$username.'.jpg';
-	
+    $cache = (strstr($_SERVER['REQUEST_URI'], '/category/')) ? '.' : '';
+    $array = split('/category/', $_SERVER['REQUEST_URI']);
+    $array = split('/', $array[1]);
+    if(count($array)!=1) $cache .= './.';
+    $cache .= './cache/';
+	$profile_image = $cache.$username.'.jpg';
+
 	// Cache the image if it doesn't already exist.
 	if (!file_exists($profile_image)) {
 	    $image_url = 'http://dropplets.com/profiles/?id='.$username.'';
 	    $image = file_get_contents($image_url);
-	    file_put_contents('./cache/'.$username.'.jpg', $image);
+	    file_put_contents($cache.$username.'.jpg', $image);
 	}
 	
 	// Return the image URL.
