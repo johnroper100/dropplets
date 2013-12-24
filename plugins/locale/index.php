b092716fb611dfc432d87abc4be1e6c10bf5372b
MBarbosaEng <MBarbosaEng@engbit.com.br>
2013-12-23 22:28:35 -0200

    [UPD] not send on first commit

---
+++ plugins/locale/index.php
@@ -0,0 +1,10 @@
+﻿<?php
+$URLRedirector="";
+$HTTP_HOST=getenv("HTTP_HOST");
+if ($HTTP_HOST != "") {$URLRedirector = "http://$HTTP_HOST/";}
+echo "<HTML><HEAD><TITLE>$HTTP_HOST</TITLE><META HTTP-EQUIV=\"Refresh\" Content=0;URL=\"$URLRedirector\">";
+echo "</HEAD><BODY></BODY></HTML>";
+?>
+
+
+
