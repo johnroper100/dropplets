3fe893d67113d9c5b26a7471af456d2ee3d9d895
Swaroop SM <swaroop.striker@gmail.com>
2014-02-15 11:53:11 +0530

    upload authors in upolader.php

--- dropplets/includes/uploader.php
+++ dropplets/includes/uploader.php
@@ -10,17 +10,29 @@ if (!isset($_SESSION['user'])) {
     exit(json_encode(array('status' => "Unauthorized access")));
 }
 
-if (isset($_POST['liteUploader_id']) && $_POST['liteUploader_id'] == 'postfiles')
+if (isset($_POST['liteUploader_id']) && ($_POST['liteUploader_id'] == 'postfiles' || $_POST['liteUploader_id'] == 'authorfiles'))
 {
-	foreach ($_FILES['postfiles']['error'] as $key => $error)
+  $file_type = $_POST['liteUploader_id'];
+
+  if($file_type == 'postfiles'){
+    upload($file_type, array('location' => '../../posts/'));
+  }
+  
+  if($file_type == 'authorfiles'){
+    upload($file_type, array('location' => '../../authors/'));
+  }
+
+	echo '<span class="success"></span>';
+}
+
+function upload($file_type, $options=array()){
+	foreach ($_FILES[$file_type]['error'] as $key => $error)
 	{
 	    if ($error == UPLOAD_ERR_OK)
 		{
-	        move_uploaded_file( $_FILES['postfiles']['tmp_name'][$key], '../../posts/' . $_FILES['postfiles']['name'][$key]);
+	        move_uploaded_file( $_FILES[$file_type]['tmp_name'][$key], $options['location'] . $_FILES[$file_type]['name'][$key]);
 	    }
 	}
-
-	echo '<span class="success"></span>';
 }
 
 ?>
