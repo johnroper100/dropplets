7c78dcd8fd0e8788be99b3353e7b1c65dc5cd013
John Roper <johnroper100@gmail.com>
2017-07-03 14:10:10 -0400

    Update php markdown extra

--- dropplets/includes/php-markdown-lib/Readme.php
+++ dropplets/includes/php-markdown-lib/Readme.php
@@ -1,18 +1,20 @@
 <?php
 
-# This file passes the content of the Readme.md file in the same directory
-# through the Markdown filter. You can adapt this sample code in any way
-# you like.
+// This file passes the content of the Readme.md file in the same directory
+// through the Markdown filter. You can adapt this sample code in any way
+// you like.
 
-# Install PSR-0-compatible class autoloader
+// Install PSR-4-compatible class autoloader
 spl_autoload_register(function($class){
-	require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+	require str_replace('\\', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
 });
+// If using Composer, use this instead:
+//require 'vendor/autoloader.php';
 
-# Get Markdown class
-use \Michelf\Markdown;
+// Get Markdown class
+use Michelf\Markdown;
 
-# Read file and pass content through the Markdown parser
+// Read file and pass content through the Markdown parser
 $text = file_get_contents('Readme.md');
 $html = Markdown::defaultTransform($text);
 
@@ -24,7 +26,7 @@ $html = Markdown::defaultTransform($text);
     </head>
     <body>
 		<?php
-			# Put HTML content in the document
+			// Put HTML content in the document
 			echo $html;
 		?>
     </body>
