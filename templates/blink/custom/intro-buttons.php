ac27e02fe9119b130f0e44569516d062f872646a
John Roper <johnroper100@gmail.com>
2018-07-20 14:50:09 -0400

    cleanup

--- templates/blink/custom/intro-buttons.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Leave this file blank if you don't want buttons in your header.
-	// Look at the examples below if you would like to add some.
-?>
-
-<!--<a href="http://jmroper.com"><li>R&eacute;sum&eacute;</li></a>
-<a href="https://github.com/johnroper100/"><li>GitHub</li></a>-->
--- templates/cards/404.php
+++
@@ -1,8 +0,0 @@
-<article class="full">
-    <div class="read">
-        <div class="post">
-            <h2><?php echo($error_title); ?></h2>
-            <p><?php echo($error_text); ?></p>
-        </div>
-    </div>
-</article>
