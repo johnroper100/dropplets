63e62255f5a9c1eedc9f06317120dd3e0b6c37c6
John Roper <johnroper100@gmail.com>
2018-08-11 21:37:15 -0400

    update gitignore

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$blogName='Test Blog';
-$blogAuthor='John Roper';
-$blogCopyright='Copyright 2018 John Roper';
-$blogPassword='$2y$10$MTFit0.b1nNfV1YfFivn6ON/F3NbykGYhHaW2npFtENNOpnxU7B86'
-?>
