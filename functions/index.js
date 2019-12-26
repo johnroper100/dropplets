288ba7359119a270899c233b4689289802879066
Vince <vincent5890@gmail.com>
2019-12-26 18:46:22 +0100

    ✨ Ajout de Firebase Hosting

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt')
+const express = require('express')
+
+const app = express()
+
+const config = {
+  dev: false
+}
+
+const nuxt = new Nuxt(config)
+
+let isReady = false
+const readyPromise = nuxt
+  .ready()
+  .then(() => {
+    isReady = true
+  })
+  .catch(() => {
+    process.exit(1)
+  })
+
+async function handleRequest(req, res) {
+  if (!isReady) {
+    await readyPromise
+  }
+  res.set('Cache-Control', 'public, max-age=1, s-maxage=1')
+  await nuxt.render(req, res)
+}
+
+app.get('*', handleRequest)
+app.use(handleRequest)
+exports.nuxtssr = functions.https.onRequest(app)
