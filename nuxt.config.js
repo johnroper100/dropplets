b4dea2c760f1ef54a7a7ab568d44b7507a845e80
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-10-25 19:09:22 +0100

    💄 Adding transitions

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['~/assets/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
