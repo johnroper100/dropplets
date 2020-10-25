50e1b180fe17d81047934f5a3d06d67bbf00998d
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-10-25 23:42:25 +0100

    ⬆️ Upgrading dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -106,7 +106,7 @@ export default {
       ],
       // by default the workbox module will not install the service worker in dev environment to avoid conflicts with HMR
       // only set this true for testing and remember to always clear your browser cache in development
-      dev: true,
+      dev: false,
     },
   },
 }
