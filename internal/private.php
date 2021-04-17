c50d08423bdf0708c11c1c885ed2166641f6eb07
John Roper <johnroper100@gmail.com>
2021-04-17 16:58:44 -0400

    update wording

--- internal/private.php
+++ internal/private.php
@@ -4,7 +4,7 @@ require "header.php";
 <h1 class="setupH1 setup">Private Post</h1>
 <form method="get" action="">
     <fieldset>
-        <legend>Type your password to access private post:</legend>
+        <legend>Type the password to access this private post:</legend>
         <input type="password" name="password" placeholder="Post password" required />
     </fieldset>
     <input class="btn" type="submit" value="Unlock" />
